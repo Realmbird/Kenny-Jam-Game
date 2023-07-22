1af4964a68db81110ed839eea488cde74c1dfb36
MlGhotdog0046 <109842656+MlGhotdog0046@users.noreply.github.com>
2023-07-22 15:53:18 -0700

    removed animatedsprite

--- Player.gd
+++ Player.gd
@@ -2,36 +2,20 @@ extends KinematicBody2D
 var health = 100
 
 
-const NEAR_DISTANCE = 50
+
 
 const ACCELERATION = 200
 const MAX_SPEED = 200
 const FRICION = 400
-var animated_sprite
+
 
 var velocity = Vector2.ZERO
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	animated_sprite = get_parent().get_node("Chest")
+	pass
 
 func _physics_process(delta):
-	  # Get the player's global position
-	var player_position = self.global_position
-
-	# Get the AnimatedSprite's global position
-	var sprite_position = animated_sprite.global_position
-
-	# Calculate the distance between the player and the AnimatedSprite
-	var distance = player_position.distance_to(sprite_position)
-
-	# Check if the player is near the AnimatedSprite
-	if distance < NEAR_DISTANCE:
-
-		if Input.is_action_just_pressed("ui_accept"):
-			print('pressed')
-			animated_sprite.play('open')
-			# Function to play the specified animation once
-
+	
 	update_health()
 
 
