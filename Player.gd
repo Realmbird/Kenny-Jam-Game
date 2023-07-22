9a7c2eacd249b164f5dd3aadbc254d0184ebb77b
MlGhotdog0046 <109842656+MlGhotdog0046@users.noreply.github.com>
2023-07-22 13:23:51 -0700

    startmenu and healthbar

---
+++ Player.gd
@@ -0,0 +1,45 @@
+extends KinematicBody2D
+var health = 100
+const ACCELERATION = 200
+const MAX_SPEED = 200
+const FRICION = 400
+
+var velocity = Vector2.ZERO
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+func _physics_process(delta):
+	update_health()
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left") 
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up") 
+	input_vector = input_vector.normalized()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICION * delta)
+	velocity = move_and_slide(velocity)
+func update_health():
+	var healthbar = $healthbar
+	print(health)
+	healthbar.set_value(health)
+	
+	if health >= 100:
+		healthbar.visible = false
+	else: 
+		healthbar.visible = true
+	
+
+
+func _on_regen_timer_timeout():
+	if health < 100:
+		health = health + 20
+		if health > 100:
+			health = 100
+	if health <= 0:
+		health = 0
+
