884d9b4bc8b9548d80ae8bfd130c6cd6f9e9d349
Realmbird <happychristopher777@gmail.com>
2023-07-22 22:58:56 -0700

    Added Ability to kill enemies

--- Attacks/Floating Sword.gd
+++ Attacks/Floating Sword.gd
@@ -1,3 +1,6 @@
 extends Node2D
 func  _ready():
 	$AnimationPlayer.play("Shoot")
+func enemyHit(area: Area2D):
+	area.get_parent().queue_free()
+
--- Enemy.gd
+++ Enemy.gd
@@ -2,3 +2,7 @@ extends Node2D
 func _physics_process(delta):
 	var player = get_parent().get_node("Player")
 	position += (player.position - position).normalized() * 0.7
+	
+
+func _on_Hitbox_area_entered(area):
+	pass # Replace with function body.
