971b2b5da129a64f659e3a488faeeec5bcecebcc
Realmbird <happychristopher777@gmail.com>
2023-07-22 19:45:47 -0700

    Added Floating Sword Slash

--- World.gd
+++ World.gd
@@ -2,4 +2,4 @@ extends YSort
 func spawn():
 	var enemy = load("res://Enemy.tscn").instance()
 	add_child(enemy)
-	enemy.position = get_node("Player").position + Vector2(1000,0).rotated(rand_range(0, 2 * PI))
+	enemy.position = get_node("Player").position + Vector2(500,0).rotated(rand_range(0, 2 * PI))
