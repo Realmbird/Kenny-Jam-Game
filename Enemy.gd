5f7645cc69d8bfd3f49799ca21dede79209ce3b2
Realmbird <happychristopher777@gmail.com>
2023-07-22 23:39:20 -0700

    Added Player Hitbox not yet implimented

--- Enemy.gd
+++ Enemy.gd
@@ -1,8 +1,11 @@
-extends Node2D
+extends KinematicBody2D
 func _physics_process(delta):
 	var player = get_parent().get_node("Player")
-	position += (player.position - position).normalized() * 0.7
+	move_and_slide((player.position - position).normalized() * 0.7)
 	
 
 func death():
 	$AnimationPlayer.play("Death")
+
+func hit_player(area):
+	pass # Replace with function body.
